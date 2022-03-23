470ed0809771f07d242ac44d5ae9a17aabfa217b
Leonardo Kasperavičius <leocck@gmail.com>
2022-03-23 19:55:11 +0100

    Implement task 2.1

--- src/main/java/com/redi/j2/Main.java
+++ src/main/java/com/redi/j2/Main.java
@@ -1,8 +1,14 @@
 package com.redi.j2;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static List<Integer> mergeLists(List<Integer> list1, List<Integer> list2) {
+        return null;
     }
 }
